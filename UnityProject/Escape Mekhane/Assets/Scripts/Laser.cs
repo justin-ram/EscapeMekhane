8750138212bfde4cf6bd6f857f5f270b28403986
justin-ram <justinisaiahlewis@gmail.com>
2026-09-12 17:32:57 -0400

    laser works now would have to set them up separately though.

--- UnityProject/Escape Mekhane/Assets/Scripts/Laser.cs
+++ UnityProject/Escape Mekhane/Assets/Scripts/Laser.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Laser : MonoBehaviour
 {
+    [Header("Scale Things")]
     [SerializeField] float scaleValue;
     [SerializeField] Transform scalePivot;
     Transform scalePivotOriginal;
+
     float scaleY;
     float scaleYOrig;
     [SerializeField] float delayTimer;
     bool scaleShrunk;
     [SerializeField] bool shouldShrink;
+
+    [Header("Rotate Things")]
+    [SerializeField] Transform rotationPivot;
+    [SerializeField] bool shouldRotate;
+    [SerializeField] float rotationX;
+  
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         scalePivotOriginal = scalePivot;
         scaleY = scalePivot.localScale.y;
         scaleYOrig = scalePivot.localScale.y;
+   
     }
 
     // Update is called once per frame
@@ -25,6 +37,7 @@ public class Laser : MonoBehaviour
         if (gameManager.instance.isPaused == false)
         {
             StartCoroutine(scaleLasers());
+            rotateLasers();
         }
 
     }
@@ -51,6 +64,16 @@ public class Laser : MonoBehaviour
                 if (scaleY > scaleYOrig)
                     scaleShrunk = false;
             }
+        }
+    }
+
+    void rotateLasers()
+    {
+        if (shouldRotate)
+        {
+
+            rotationPivot.Rotate(rotationX, 0, 0);
+
         }
     }
 
