91ef23096c239f5e9480189cd0b2507414c86b64
justin-ram <justinisaiahlewis@gmail.com>
2026-09-12 13:40:08 -0400

    Laser trap able to be scaled up and down.

--- UnityProject/Escape Mekhane/Assets/Scripts/Laser.cs
+++ UnityProject/Escape Mekhane/Assets/Scripts/Laser.cs
@@ -1,16 +1,58 @@
+using System.Collections;
 using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
+    [SerializeField] float scaleValue;
+    [SerializeField] Transform scalePivot;
+    Transform scalePivotOriginal;
+    float scaleY;
+    float scaleYOrig;
+    [SerializeField] float delayTimer;
+    bool scaleShrunk;
+    [SerializeField] bool shouldShrink;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        scalePivotOriginal = scalePivot;
+        scaleY = scalePivot.localScale.y;
+        scaleYOrig = scalePivot.localScale.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameManager.instance.isPaused == false)
+        {
+            StartCoroutine(scaleLasers());
+        }
+
     }
+
+    IEnumerator scaleLasers()
+    {
+        if (shouldShrink)
+        {
+            if (scaleY > 0.1 && !scaleShrunk)
+            {
+                scaleY -= scaleValue;
+                scalePivot.localScale = new Vector3(scalePivot.localScale.x, scaleY, scalePivot.localScale.z);
+            }
+            else
+            {
+                scaleShrunk = true;
+            }
+            yield return new WaitForSeconds(delayTimer);
+
+            if (scaleShrunk)
+            {
+                scaleY += scaleValue;
+                scalePivot.localScale = new Vector3(scalePivot.localScale.x, scaleY, scalePivot.localScale.z);
+                if (scaleY > scaleYOrig)
+                    scaleShrunk = false;
+            }
+        }
+    }
+
+
 }
